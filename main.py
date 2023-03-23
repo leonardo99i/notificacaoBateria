92a203a84c8c021b0e560513d060e682bddb3213
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2023-03-22 21:05:25 -0300

    ultima versão, ainda não funciona a lib

--- main.py
+++ main.py
@@ -1,31 +1,58 @@
-import wmi
-import psutil
-import pyautogui
+import bluetooth
 import time
+from win10toast import ToastNotifier
 
-def is_bluetooth_connected(device_name):
-    wmi_obj = wmi.WMI()
-    for controller in wmi_obj.query("SELECT * FROM Win32_PnPEntity WHERE Name LIKE '%(COM%'"):
-        if device_name in controller.Description:
-            for connected_device in controller[0].Associators_():
-                if connected_device.ClassGuid == "{e0cbf06c-cd8b-4647-bb8a-263b43f0f974}":
-                    return True
-    return False
+# Endereços MAC dos dispositivos Bluetooth a serem conectados
+addrs = ['11:22:33:44:55:66', '77:88:99:AA:BB:CC', 'DD:EE:FF:00:11:22']
 
-def main():
-    devices = {
-        "MX Master 3": "mouse",
-        "MX Keys Mini": "keyboard",
-        "AirPods de Leonardo": "headphones"
-    }
+# UUID do perfil de bateria (Battery Service) Bluetooth
+uuid = '0000180f-0000-1000-8000-00805f9b34fb'
 
-    while True:
-        for device_name, device_type in devices.items():
-            if is_bluetooth_connected(device_name):
-                battery = psutil.sensors_battery()
-                message = f"{device_type.title()} battery: {battery.percent}%"
-                pyautogui.alert(message, title="Battery Alert", button="OK")
-        time.sleep(3600) # 1 hour
+# Intervalo de tempo para verificar a porcentagem de bateria (em segundos)
+interval = 600 # 10 minutos
 
-if __name__ == "__main__":
-    main()
+# Loop infinito para verificar a porcentagem de bateria e enviar a notificação
+while True:
+    for addr in addrs:
+        # Conectando ao dispositivo Bluetooth
+        sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        sock.connect((addr, 1))
+
+        # Obtendo o nível de bateria do perfil de bateria Bluetooth
+        level = None
+        for svc in bluetooth.find_service(uuid=uuid, address=addr):
+            for chn in svc['channels']:
+                try:
+                    # Lendo o valor da porcentagem de bateria
+                    sock.send(chr(0x16))
+                    sock.send(chr(chn))
+                    data = sock.recv(1024)
+                    level = ord(data[-1])
+                except:
+                    pass
+
+        # Fechando a conexão Bluetooth
+        sock.close()
+
+        # Enviando a notificação de desktop com o nível de bateria ou com a mensagem de erro, conforme o caso
+        toaster = ToastNotifier()
+        device_name = ''
+        if addr == '11:22:33:44:55:66':
+            device_name = 'MX Keys Mini'
+        elif addr == '77:88:99:AA:BB:CC':
+            device_name = 'MX Master 3'
+        elif addr == 'DD:EE:FF:00:11:22':
+            device_name = 'Fone de ouvido'
+        if level is not None:
+            toaster.show_toast(
+                'Nível de Bateria Bluetooth - ' + device_name,
+                'O dispositivo Bluetooth ' + device_name + ' está com %d%% de carga na bateria.' % level,
+                duration=10)
+        else:
+            toaster.show_toast(
+                'Erro de Leitura Bluetooth - ' + device_name,
+                'Não foi possível obter a porcentagem de bateria do dispositivo Bluetooth ' + device_name + '.',
+                duration=10)
+
+    # Aguardando o intervalo de tempo definido
+    time.sleep(interval)
